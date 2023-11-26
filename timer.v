cc2e1df8d95c767800540c49dbc33498aa9db56e
youssefalbertt <youssefalbert2003@gmail.com>
2023-11-26 02:49:00 +0200

    timer module

---
+++ timer.v
@@ -0,0 +1,33 @@
+module timer (
+    input wire clk,
+    input wire rst,
+    input wire start,
+    input wire [31:0] threshold,
+    output reg time_out
+
+);
+
+reg[31:0] counter;
+
+always @(posedge clk or negedge rst)
+    begin 
+        if (!rst)
+        begin
+            counter <= 1'b0;
+            time_out <= 1'b0;
+        end
+        else if (start)
+        begin
+            counter <= counter + 1'b1;
+        end
+
+        if (counter == threshold)
+        begin
+            time_out <= 1'b1;
+            counter <= 1'b0;
+        end
+    end
+endmodule 
+
+    
+            
