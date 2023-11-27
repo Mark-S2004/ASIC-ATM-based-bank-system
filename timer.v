c15bc1912946fc0f4f5e557e422163b4c40a2ad6
Youssef-Hany1 <21p0082@eng.asu.edu.eg>
2023-11-27 15:04:23 +0200

    Develop timer module

--- timer.v
+++ timer.v
@@ -2,6 +2,7 @@ module timer (
     input wire clk,
     input wire rst,
     input wire start,
+    input wire restart,
     input wire [31:0] threshold,
     output reg time_out
 
@@ -18,15 +19,24 @@ always @(posedge clk or negedge rst)
         end
         else if (start)
         begin
+            if (restart)
+            begin
+            counter <= 1'b0;
+            end
+            else
+            begin
             counter <= counter + 1'b1;
+            end
+            if (counter == threshold)
+            begin
+               time_out <= 1'b1;
+               counter <= 1'b0;
+            end
         end
-
-        if (counter == threshold)
+        else
         begin
-            time_out <= 1'b1;
             counter <= 1'b0;
         end
-    end
 endmodule 
 
     
