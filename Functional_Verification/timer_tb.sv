91b34040d49c13b5c6f1d42cd11beba2e8d53eca
Mark Saleh <mark-saleh@outlook.com>
2023-12-03 21:42:01 +0200

    Add coverage and PSL assertions to timer testbench

--- Functional_Verification/timer_tb.sv
+++ Functional_Verification/timer_tb.sv
@@ -2,8 +2,9 @@ module timer_tb;
     reg clk, rst, start, restart;
     reg [31:0] threshold;
     wire timeout;
+    integer i;
 
-    timer t1(clk, rst, start, restart, threshold, timeout);
+    timer t1(clk, rst, start, restart, timeout);
 
     initial begin
         clk = 0;
@@ -14,7 +15,6 @@ module timer_tb;
         rst = 0;
         start = 0;
         restart = 0;
-        threshold = 32'd10;
         @(negedge clk);
         if (timeout == 1) $display("Error in rst");
         else $display("success");
@@ -26,22 +26,17 @@ module timer_tb;
         if (timeout == 1) $display("Error in restart");
         else $display("success");
 
-        restart = 0;
-        repeat (10) @(negedge clk);
-        if (timeout == 0) $display("Error in counter 10");
-        else $display("success");
-
         start = 0;
         @(negedge clk);
         start = 1;
         @(negedge clk);
         repeat (5) @(negedge clk);
-        if (timeout == 1) $display("Error in counter 5");
+        if (timeout) $display("Error in counter 5");
         else $display("success");
 
         restart = 0;
         repeat (11) @(negedge clk);
-        if (timeout == 0) $display("Error in counter 11");
+        if (!timeout) $display("Error in counter 11");
         else $display("success");
 
         start = 0;
@@ -49,13 +44,20 @@ module timer_tb;
         start = 1;
         @(negedge clk);
         repeat (15) @(negedge clk);
-        if (timeout == 0) $display("Error in counter 15");
+        if (!timeout) $display("Error in counter 15");
         else $display("success");
+
+        for (i = 0; i < 1000; i=i+1) begin
+            rst = $random();
+            start = $random();
+            restart = $random();
+            repeat (12) @(negedge clk);
+        end
 
         $stop();
     end
 
-    initial begin
-        $monitor("rst = %b, start = %b, restart = %b, timeout = %b", rst, start, restart, restart, timeout);
-    end
+    // psl start_assertion: assert always( (start && !restart) -> next[11] (timeout) abort !rst) @(posedge clk);
+    // psl restart_assertion: assert always( (restart) -> next (prev(timeout) == timeout) abort !rst) @(posedge clk);
+    // psl rst_assertion: assert always( (!rst) -> next (!timeout)) @(posedge clk);
 endmodule
