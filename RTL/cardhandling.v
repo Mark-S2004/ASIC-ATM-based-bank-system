96d35ab7410861cc0c0bca694e9fb0235f9bd488
Geno212 <ahmed8811223344@gmail.com>
2023-12-08 20:36:40 +0200

    remove card_out and pass_en

--- RTL/cardhandling.v
+++ RTL/cardhandling.v
@@ -7,20 +7,17 @@ module cardhandling #(parameter    card_width   = 6,
     input wire rst,
     input wire [card_width-1:0] card_number,
     input wire card_in,
-    input wire card_out,
     input wire op_done,
     input wire [balance_width-1:0] updated_balance,
     input wire [password_width-1:0] password_input,
-    
     output reg [balance_width-1:0] balance,
-    output reg psw_en,
     output reg wrong_psw
 
 );
 
 reg [password_width-1:0] password_reg [0:users_num-1] ;
 reg [balance_width-1:0] balance_reg  [0:users_num-1] ;
-reg [password_width-1:0] password;
+
 
 
 always @(posedge clk or negedge rst)
@@ -31,41 +28,32 @@ begin
         $readmemb("./Database/balance_memory.txt" , balance_reg);
 
         balance <= 1'b0;
+        wrong_psw <= 1'b0;
     end
     else 
     begin
+        wrong_psw <= 1'b0;
         if(card_number < users_num)
         begin
             if (card_in)
             begin
-                password <= password_reg[card_number] ;
-                balance  <= balance_reg[card_number]  ;
-                if(password_input != password)
+            
+                balance <= balance_reg[card_number]  ;
+                if(password_input != password_reg[card_number])
                     wrong_psw <= 1'b1;
             end
-            if (card_out || op_done)
+            if ((!card_in)|| op_done)
             begin
                 balance_reg[card_number] <= updated_balance;
+                $writememb("./Database/balance_memory.txt",balance_reg,0,users_num-1);
             end
         end
         else 
         begin
             balance <= 1'b0;
+            wrong_psw <= 1'b0;
         end
     end
 end
-
-always @(posedge clk or negedge rst)
-begin
-    if (!rst)
-    begin
-        psw_en <= 1'b0;
-    end
-    else if(card_number < users_num)
-    begin
-        psw_en <= card_in;
-    end
-end
-
 
 endmodule
