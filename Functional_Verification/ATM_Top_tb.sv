d68a29c0b824ba8791ea44b48c042cbd41e1fa57
Mark Saleh <mark-saleh@outlook.com>
2023-12-16 13:11:44 +0200

    Fix Top module wrong_psw and inquiry assertions

--- Functional_Verification/ATM_Top_tb.sv
+++ Functional_Verification/ATM_Top_tb.sv
@@ -2,13 +2,13 @@ module ATM_Top_tb #(parameter   password_width = 4,
                                 balance_width = 20, 
 							    card_width = 3,
                                 users_num = 7);
-    reg clk, rst, language, another_service, card_in_reg;	
+    reg clk, rst, language, another_service, card_out_reg;	
     reg [card_width-1:0] card_number;
     reg [password_width-1:0] password_input;
     reg [1:0] operation;
     reg [balance_width-1:0] value;
 	reg [31:0] threshold;
-    wire card_in, op_done, error, wrong_psw;
+    wire card_out, op_done, error, wrong_psw;
     wire [balance_width-1:0] updated_balance;
     integer i;
 
@@ -17,7 +17,7 @@ module ATM_Top_tb #(parameter   password_width = 4,
 
     ATM_Top top
     (
-        clk, rst, card_number, password_input, card_in, language, operation, value, another_service, updated_balance, op_done, error, wrong_psw
+        clk, rst, card_number, password_input, card_out, language, operation, value, another_service, updated_balance, op_done, error, wrong_psw
     );
 
     initial begin
@@ -26,27 +26,27 @@ module ATM_Top_tb #(parameter   password_width = 4,
     end
 
     initial begin
-        $readmemb("./Database/balance_memory.txt" , balance_reg);
         $readmemb("./Database/password_memory.txt", password_reg);
         rst = 0;
         @(negedge clk);
         for (i = 0; i < 1000; i=i+1) begin
+            $readmemb("./Database/balance_memory.txt" , balance_reg);
             rst = $random();
             card_number = $random();
-            card_in_reg = $random();
+            card_out_reg = $random();
             password_input = $random();
             language = $random();
             operation = $random();
             value = $random();
             another_service = $random();
-            @(negedge clk);
-            $readmemb("./Database/balance_memory.txt" , balance_reg);
+            repeat (10) @(negedge clk);
         end
         $stop();
     end
-    assign card_in = card_in_reg;
+    assign card_out = card_out_reg;
 
-    // psl rst_assertion: assert always( (!rst) -> next (!op_done && !error && !wrong_psw) ) @(posedge clk);
-    // psl wrong_psw_assertion: assert always( (card_number < users_num && password_input != password_reg[card_number] && card_in) -> next (wrong_psw) abort !rst) @(posedge clk);
-    // psl inquiry_assertion: assert always( (card_number < users_num && card_in && operation == 2'b11 && !another_service) -> next (updated_balance == balance_reg[card_number]) abort !rst) @(posedge clk);
+    // psl rst_assertion: assert always( (!rst) -> next (!updated_balance && !op_done && !error && !wrong_psw) ) @(posedge clk);
+    // psl wrong_psw_assertion: assert always( (password_input != password_reg[card_number] && !card_out) -> next (wrong_psw) abort !rst) @(posedge clk);
+    // psl inquiry_assertion: assert always( (password_input == password_reg[card_number] && !card_out && operation == 2'b10 && !another_service) -> next[5] (updated_balance == balance_reg[card_number] && op_done && !error && !wrong_psw) abort !rst) @(posedge clk);
+    // psl assert always (never (card_number < users_num));
 endmodule
