a4a911b51990470f452fc1068eb4e68d0a5cea90
Mark Saleh <mark-saleh@outlook.com>
2023-12-16 17:52:35 +0200

    Improve FSM testbench psl assertions [*k]

--- Functional_Verification/ATM_FSM_tb.sv
+++ Functional_Verification/ATM_FSM_tb.sv
@@ -48,12 +48,215 @@ module ATM_FSM_tb #(parameter balance_width = 20);
             value = $random();
             repeat (10) @(negedge clk);
         end
+/*
+        rst = 0;
+        @(negedge clk);
+        rst = 1;
+        timeout = 0;
+        wrong_psw = 0;
+        language = 0;
+        another_service = 1;
+        current_balance = 20'd1000;
+        operation = 2'b01;
+        value = 20'd200;
+        
+        repeat (10) @(negedge clk);;
+
+        operation = 2'b01;
+        value = 200;
+        another_service = 0;
+        current_balance = 20'd1200;
+        repeat (10) @(negedge clk);
+        if (op_done && balance == 20'd1400 )
+            $display("Done");
+        else
+            $display("Fail");
+          // DW
+        rst = 1;
+        timeout = 0;
+        wrong_psw = 0;
+        language = 0;
+        another_service = 1;
+        current_balance = 20'd1000;
+        operation = 2'b01;
+        value = 20'd200;
+        
+        repeat (10) @(negedge clk);;
+
+        operation = 2'b00;
+        value = 200;
+        another_service = 0;
+        current_balance = 20'd1200;
+        repeat (10) @(negedge clk);
+        if (op_done && balance == 20'd1000 )
+            $display("Done");
+        else
+            $display("Fail");
+        // DI
+        rst = 1;
+        timeout = 0;
+        wrong_psw = 0;
+        language = 0;
+        another_service = 1;
+        current_balance = 20'd1000;
+        operation = 2'b01;
+        value = 20'd200;
+        
+        repeat (10) @(negedge clk);;
+
+        operation = 2'b10;
+        value = 200;
+        another_service = 0;
+        current_balance = 20'd1200;
+        repeat (10) @(negedge clk);
+        if (op_done && balance == 20'd1200 )
+            $display("Done");
+        else
+            $display("Fail");
+        
+        // WW
+        rst = 1;
+        timeout = 0;
+        wrong_psw = 0;
+        language = 0;
+        another_service = 1;
+        current_balance = 20'd1000;
+        operation = 2'b00;
+        value = 20'd200;
+        
+        repeat (10) @(negedge clk);;
+
+        operation = 2'b00;
+        value = 200;
+        another_service = 0;
+        current_balance = 20'd800;
+        repeat (10) @(negedge clk);
+        if (op_done && balance == 20'd600 ) 
+            $display("Done");
+        else
+            $display("Fail");
+        // WD
+        rst = 1;
+        timeout = 0;
+        wrong_psw = 0;
+        language = 0;
+        another_service = 1;
+        current_balance = 20'd1000;
+        operation = 2'b00;
+        value = 20'd200;
+        
+        repeat (10) @(negedge clk);;
+
+        operation = 2'b01;
+        value = 200;
+        another_service = 0;
+        current_balance = 20'd800;
+        repeat (10) @(negedge clk);
+        if (op_done && balance == 20'd1000 ) 
+            $display("Done");
+        else
+            $display("Fail");
+        
+        // WI
+        rst = 1;
+        timeout = 0;
+        wrong_psw = 0;
+        language = 0;
+        another_service = 1;
+        current_balance = 20'd1000;
+        operation = 2'b00;
+        value = 20'd200;
+        
+        repeat (10) @(negedge clk);;
+
+        operation = 2'b10;
+        value = 200;
+        another_service = 0;
+        current_balance = 20'd800;
+        repeat (10) @(negedge clk);
+        if (op_done && balance == 20'd800 ) 
+            $display("Done");
+        else
+            $display("Fail");
+        
+        // II
+        rst = 1;
+        timeout = 0;
+        wrong_psw = 0;
+        language = 0;
+        another_service = 1;
+        current_balance = 20'd1000;
+        operation = 2'b10;
+        value = 20'd200;
+        
+        repeat (10) @(negedge clk);;
+
+        operation = 2'b10;
+        value = 200;
+        another_service = 0;
+        current_balance = 20'd1000;
+        repeat (10) @(negedge clk);
+        if (op_done && balance == 20'd1000)
+            $display("Done");
+        else
+            $display("Fail");
+        
+        // IW
+        rst = 1;
+        timeout = 0;
+        wrong_psw = 0;
+        language = 0;
+        another_service = 1;
+        current_balance = 20'd1000;
+        operation = 2'b10;
+        value = 20'd200;
+        
+        repeat (10) @(negedge clk);;
+
+        operation = 2'b00;
+        value = 200;
+        another_service = 0;
+        current_balance = 20'd1000;
+        repeat (10) @(negedge clk);
+        if (op_done && balance == 20'd800 )
+            $display("Done");
+        else
+            $display("Fail");
+        
+        // ID
+        rst = 1;
+        timeout = 0;
+        wrong_psw = 0;
+        language = 0;
+        another_service = 1;
+        current_balance = 20'd1000;
+        operation = 2'b10;
+        value = 20'd200;
+        
+        repeat (10) @(negedge clk);;
+
+        operation = 2'b01;
+        value = 200;
+        current_balance = 20'd1000;
+        repeat (10) @(negedge clk);
+        if (op_done && balance == 20'd1200 ) 
+            $display("Done");
+        else
+            $display("Fail op_done: %b, balance: %d", op_done, balance);
+        another_service = 0;
+*/
         $stop();
     end
 
     // psl rst_assertion: assert always( (!rst) -> next (!balance && !error && !op_done) ) @(posedge clk);
-    // psl withdraw_assertion: assert always( (!wrong_psw && operation == 2'b00 && !another_service && !timeout && value < current_balance) -> next[5] (balance == current_balance - value && op_done && card_out && !error) abort !rst) @(posedge clk);
-    // psl invalid_withdraw_assertion: assert always( (!wrong_psw && operation == 2'b00 && !another_service && !timeout && value > current_balance) -> next[9] (balance == current_balance && op_done && card_out && error) abort !rst) @(posedge clk);
-    // psl deposit_assertion: assert always( (!wrong_psw && operation == 2'b01 && !another_service && !timeout) -> next[5] (balance == current_balance + value && op_done && card_out && !error) abort !rst) @(posedge clk);
-    // psl inquiry_assertion: assert always( (!wrong_psw && operation == 2'b10 && !another_service && !timeout) -> next[5] (balance == current_balance && op_done && card_out && !error) abort !rst) @(posedge clk);
+
+    // psl withdraw_assertion: assert always( {(!wrong_psw && operation == 2'b00 && !another_service && !timeout && value < current_balance)[*10]} (balance == current_balance - value && op_done && card_out && !error) abort !rst) @(posedge clk);
+
+    // psl invalid_withdraw_assertion: assert always( {(!wrong_psw && operation == 2'b00 && !another_service && !timeout && value > current_balance)[*10]} (balance == current_balance && op_done && card_out && error) abort !rst) @(posedge clk);
+
+    // psl deposit_assertion: assert always( {(!wrong_psw && operation == 2'b01 && !another_service && !timeout)[*10]} (balance == current_balance + value && op_done && card_out && !error) abort !rst) @(posedge clk);
+
+    // psl inquiry_assertion: assert always( {(!wrong_psw && operation == 2'b10 && !another_service && !timeout)[*10]} (balance == current_balance && op_done && card_out && !error) abort !rst) @(posedge clk);
+
+    // psl assert always( never (value < 20'd0) );
 endmodule
